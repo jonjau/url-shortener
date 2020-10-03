af7f4b7775a7c006349db1fb75104830b4a595e4
jonjau <jonjau3Q@gmail.com>
2020-10-03 20:08:48 +1000

    working

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const fs = require("fs");
+
+const ShortURL = require("./models/url");
+
 const app = express();
 const PORT = 3000;
-const CONNECTION = fs.readFileSync('mongo.config', 'utf8')
+const CONNECTION = fs.readFileSync("mongo.config", "utf8");
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const allData = await ShortURL.find();
+  res.render("index", { shortUrls: allData });
 });
 
-app.post("/short", (req, res) => {
-  const db = mongoose.connection.db;
-  // insert record in 'test' collection
-  db.collection("test").insertOne({ testCompleted: 1 });
+app.get("/:shortid", async (req, res) => {
+  // grab the :shortid param
+  const shortid = req.params.shortid;
 
-  res.json({ ok: 1 });
+  // perform the mongoose call to find the long URL
+  const rec = await ShortURL.findOne({ short: shortid });
+
+  // if null, set status to 404 (res.sendStatus(404))
+  if (!rec) return res.sendStatus(404);
+
+  // if not null, increment the click count in database
+  rec.clicks++;
+  await rec.save();
+
+  // redirect the user to original link
+  res.redirect(rec.full);
 });
 
-app.get("/short", (req, res) => {
-  res.send("Hello from short");
+app.post("/short", async (req, res) => {
+  // Grab the fullUrl parameter from the req.body
+  const fullUrl = req.body.fullUrl;
+  console.log("URL requested: ", fullUrl);
+
+  // insert and wait for the record to be inserted using the model
+  const record = new ShortURL({
+    full: fullUrl,
+  });
+
+  await record.save();
+
+  res.redirect("/");
 });
 
 // setup mongodb connection
 mongoose.connect(CONNECTION, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-mongoose.connection.on('open', () => {
+mongoose.connection.on("open", async () => {
   // wait for mongodb connection before server starts
+
+  await ShortURL.create({ full: "http://google.com" });
+  await ShortURL.create({ full: "http://duckduckgo.com" });
+
   app.listen(PORT, () => {
     console.log(`Server started on: ${PORT}`);
-  })
+  });
 });
